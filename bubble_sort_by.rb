ba2dd7cd71a1153769542b5f6dd5bd93d7cdea88
Meltrust <original.mtapia@outlook.com>
2020-05-21 11:24:33 -0500

    correct bubble sory by name

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,14 +1,14 @@
-def bubble_sort_by(a)
-  a.each do
+def bubble_sort_by(my_var)
+  my_var.each do
     x = 0
     y = 1
-    until y == (a.size)
-      comparison = yield(a[x], a[y])
+    until y == (my_var.size)
+      comparison = yield(my_var[x], my_var[y])
       if comparison.positive?
-        a.insert(x, a.delete_at(y))
+        my_var.insert(x, my_var.delete_at(y))
         puts "ordering"
         puts ""
-        puts a
+        puts my_var
         puts ""
       end
      x = x + 1
@@ -16,5 +16,5 @@ def bubble_sort_by(a)
    end
   end
 end
-a = %w[hello friends worlds hi complicated bye]
-print bubble_sort_by(a) { |str1, str2| str1.length - str2.length}
+my_var = %w[hello friends worlds hi complicated bye]
+print bubble_sort_by(my_var) { |str1, str2| str1.length - str2.length}
