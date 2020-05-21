2df56a28ce35a45eb31b34573381f9d11afe5a64
Meltrust <original.mtapia@outlook.com>
2020-05-21 11:03:18 -0500

    add solution

---
+++ bubble_sort.rb
@@ -0,0 +1,32 @@
+def bubble_sort ()
+  puts "size of array:"
+  size = gets.chomp.to_i
+  puts ""
+  puts "max values in array"
+  max_values = gets.chomp.to_i
+  puts ""
+
+  a = size.times.map{Random.rand(max_values)}
+
+  puts "original array: #{a}"
+  puts ""
+
+  a.each do
+    x = 0
+    y = 1
+    until y == (a.size)
+      if a[x] > a[y]
+        a.insert(x, a.delete_at(y))
+        puts "ordering"
+        puts ""
+        puts a
+        puts ""
+      end
+      x = x + 1
+      y = y + 1
+    end
+  end
+end
+
+puts bubble_sort()
+
