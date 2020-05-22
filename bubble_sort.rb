dc702d9cf8947b0829b1ccbeb12431ee23cc50f3
Miguel Tapia <original.mtapia@outlook.com>
2020-05-22 13:52:30 -0500

    correct linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,7 +3,7 @@ def bubble_sort(unsorted)
     x = 0
     y = 1
     until y == unsorted.size
-      unsorted.insert(x, unsorted.delete_at(y)) if unsorted[x] > unsorted[y] 
+      unsorted.insert(x, unsorted.delete_at(y)) if unsorted[x] > unsorted[y]
       x += 1
       y += 1
     end
