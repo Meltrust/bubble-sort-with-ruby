646a115a53ffcaf48ecd6795edf824d0ce6a580b
Miguel Tapia <original.mtapia@outlook.com>
2020-05-22 09:40:06 -0500

    Update bubble_sort_by.rb

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -6,10 +6,6 @@ def bubble_sort_by(my_var)
       comparison = yield(my_var[x], my_var[y])
       if comparison.positive?
         my_var.insert(x, my_var.delete_at(y))
-        puts 'ordering'
-        puts ''
-        puts my_var
-        puts ''
       end
       x += 1
       y += 1
